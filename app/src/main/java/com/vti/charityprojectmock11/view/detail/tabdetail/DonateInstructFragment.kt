d1059a7a598d4ddc53f80ab70af1acda8021165d
NorthBTB <bacbieber000@gmail.com>
2021-12-18 21:24:02 +0700

    detail program fragment

--- app/src/main/java/com/vti/charityprojectmock11/view/detail/tabdetail/DonateInstructFragment.kt
+++ app/src/main/java/com/vti/charityprojectmock11/view/detail/tabdetail/DonateInstructFragment.kt
@@ -1,10 +1,16 @@
 package com.vti.charityprojectmock11.view.detail.tabdetail
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.vti.charityprojectmock11.R
 import com.vti.charityprojectmock11.databinding.FragmentDonateInstructBinding
@@ -31,6 +37,34 @@ class DonateInstructFragment : Fragment() {
         viewModel.donateProgram.observe(viewLifecycleOwner, { donateProgram ->
             binding.txtTitle.text = donateProgram.content
         })
+
+        binding.btnDonate.setOnClickListener{
+            if (binding.etMoney.text.isEmpty()) {
+                Toast.makeText(activity, "Hãy nhập số tiền quyên góp", Toast.LENGTH_SHORT)
+            } else {
+                showDialog(binding.etMoney.text.toString())
+            }
+        }
+    }
+
+    private fun showDialog(title: String) {
+        val dialog = Dialog(activity!!)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.dialog)
+        val body = dialog.findViewById(R.id.tv_message) as TextView
+        body.text = "Bạn muốn quyên góp " + title + " vnđ ?"
+        val confirmBtn = dialog.findViewById(R.id.btn_confirm) as Button
+        val icon = dialog.findViewById(R.id.iv_icon) as ImageView
+        icon.setImageResource(R.drawable.ic_baseline_error_outline_24)
+        confirmBtn.setOnClickListener {
+            Toast.makeText(
+                activity!!,
+                "Cảm ơn bạn đã ủng hộ chuong trình " + title + " vnđ",
+                Toast.LENGTH_LONG
+            )
+            dialog.dismiss()
+        }
     }
 
 }
--- app/src/main/java/com/vti/charityprojectmock11/view/detail/tabdetail/ProgramFragment.kt
+++ app/src/main/java/com/vti/charityprojectmock11/view/detail/tabdetail/ProgramFragment.kt
@@ -1,22 +1,104 @@
 package com.vti.charityprojectmock11.view.detail.tabdetail
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.vti.charityprojectmock11.R
+import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
+import com.vti.charityprojectmock11.adapter.ViewPager2Adapter
+import com.vti.charityprojectmock11.databinding.FragmentProgramBinding
+import com.vti.charityprojectmock11.model.DonateProgram
+import com.vti.charityprojectmock11.viewmodel.detail.DetailDonateViewModel
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class ProgramFragment : Fragment() {
+
+    private val viewModel: DetailDonateViewModel by viewModels(ownerProducer = { requireParentFragment() })
+    private lateinit var binding: FragmentProgramBinding
+
+    private val imageList = mutableListOf<String>()
+    private var currentIndex: Int = 0
+
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_program, container, false)
+        binding = FragmentProgramBinding.inflate(inflater, container, false)
+
+        binding.vpProgramImg.adapter = ViewPager2Adapter(imageList, requireContext())
+        binding.vpProgramImg.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        binding.indicatorProgam.setViewPager(binding.vpProgramImg)
+        //auto slide image
+        mHandler = Handler()
+        mRunnable = Runnable {
+            if ((currentIndex + 1) > imageList.size) {
+                currentIndex = 0;
+            } else {
+                currentIndex++;
+            }
+            binding.vpProgramImg.setCurrentItem(currentIndex);
+            mHandler.postDelayed(
+                mRunnable, // Runnable
+                3 * 1000 // Delay in milliseconds
+            )
+        }
+        // Schedule the task to repeat after 3 second
+        mHandler.postDelayed(
+            mRunnable, // Runnable
+            3 * 1000 // Delay in milliseconds
+        )
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        if (viewModel.donateProgram != null) {
+            viewModel.donateProgram.observe(viewLifecycleOwner, { donateProgram ->
+                putToList(donateProgram)
+                binding.tvContent.text = donateProgram.content
+                binding.tvTitle.text = donateProgram.name
+                binding.tvProcess.text =
+                    donateProgram.currentMoney.toString() + "/" + donateProgram.targetMoney.toString()
+                binding.pbProgramProcess.progress =
+                    (donateProgram.currentMoney!!.div(donateProgram.targetMoney!!)
+                        .times(100)).toInt()
+                binding.tvCurrentDonationAmount.text =
+                    (donateProgram.currentMoney!!.div(donateProgram.targetMoney!!)
+                        .times(100)).toInt().toString() + "%"
+//                if (Calendar.getInstance().time.after(donateProgram.endDate)) {
+//                    binding.tvExpireTime.text = "Đã kết thúc"
+//                } else {
+//                    var diff: Long = donateProgram.endDate!!.time - Calendar.getInstance().time.time
+//                    binding.tvExpireTime.text =
+//                        TimeUnit.MICROSECONDS.toDays(diff).toString() + " ngày"
+//                }
+            })
+        }
+        if (viewModel.donate != null) {
+            viewModel.donate.observe(viewLifecycleOwner, { donate ->
+                binding.tvDonationAmount.text = donate.size.toString()
+            })
+        }
+    }
+
+    private fun putToList(donateProgram: DonateProgram) {
+        //call api get image
+        imageList.add(donateProgram.subImage1.toString())
+        imageList.add(donateProgram.subImage2.toString())
+        imageList.add(donateProgram.subImage3.toString())
+//        addToList(R.drawable.ic_launcher_foreground)
+//        addToList(R.drawable.ic_baseline_supervised_user_circle_24)
+//        addToList(R.drawable.ic_dashboard_black_24dp)
+//        addToList(R.drawable.ic_notifications_black_24dp)
+    }
 }
